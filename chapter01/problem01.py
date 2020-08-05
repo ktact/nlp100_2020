3cd0faba4186b2830878eeee52108f12a54e7f9b
K Tact <ktact.dev@gmail.com>
2020-08-05 22:10:56 +0900

    01. 「パタトクカシーー」を解いた

---
+++ chapter01/problem01.py
@@ -0,0 +1,5 @@
+def join_1st_3rd_5th_7th_char(str):
+    return str[0] + str[2] + str[4] + str[6]
+
+if __name__ == "main":
+    print(join_1st_3rd_5th_7th_char("パタトクカシーー"))
---
+++ chapter01/test_01.py
@@ -0,0 +1,4 @@
+from problem01 import join_1st_3rd_5th_7th_char
+
+def test():
+    assert join_1st_3rd_5th_7th_char("パタトクカシーー") == "パトカー"
